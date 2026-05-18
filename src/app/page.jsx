2015583dd25234e353054061a70ddb4996cc0806
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-05-18 16:57:05 +0600

    Who we are section done with responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Banner from '@/components/homepage/Banner';
+import WhoWeAre from '@/components/homepage/WhoWeAre';
 
 export default function Home() {
   return (
     <div>
       <Banner/>
-
+      <WhoWeAre/>
       
     </div>
   );
---
+++ src/components/homepage/WhoWeAre.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import img1 from '../../assets/who1.jpg'
+import img2 from '../../assets/who2.jpg'
+
+const WhoWeAre = () => {
+    return (
+        <div className='pt-10 md:pt-20 container mx-auto'>
+
+            <div className='flex flex-col-reverse md:flex-row justify-center gap-15'>
+                <div className='md:pt-10 m-5 space-y-3'>
+                    <p className='font-medium text-lg text-center md:text-left'>Our Ground</p>
+                    <Image src={img1} alt='Image 1' height={800} width={450} className='shadow-xl rounded-lg border border-black/20' />
+                </div>
+
+                <div className='space-y-3 m-5'>
+                    <p className='font-medium text-lg'>Who We Are</p>
+                    <h2 className='text-5xl font-bold'>BUILT FOR PASSIONATE PLAYERS <br /> AND ACTIVE COMMUNITIES</h2>
+                    <p className='max-w-xl font-medium'>We are a modern sports facility booking platform dedicated to making sports more accessible, organized, and convenient for everyone. From football turfs and badminton courts to swimming lanes and tennis arenas, our platform helps players discover, explore, and reserve facilities with ease. Whether you are booking for practice sessions, friendly matches, or professional training, we provide a smooth and reliable experience for every sports enthusiast.</p>
+
+                    <Image src={img2} alt='Image 2' height={500} width={350} className='shadow-xl rounded-lg border border-black/20 mx-auto md:mx-0' />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhoWeAre;
